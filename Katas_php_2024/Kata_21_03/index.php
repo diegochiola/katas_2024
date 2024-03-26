68172c4af8089ec1cee740a0a8efa41832975cb8
diega <diegochiola@gmail.com>
2024-03-26 10:11:28 +0100

    add terminal dialog

--- Katas_php_2024/Kata_21_03/index.php
+++ Katas_php_2024/Kata_21_03/index.php
@@ -5,6 +5,26 @@
 $numeroUsuario=153;
 $cifras=0;
 
+//ingresar el numero por temrinal:
+
+do{
+    $opcion = readline("Bienvenido a numero de Amstrong.".PHP_EOL.
+                    "Digite una opcion: ".PHP_EOL.
+                    " 1. Jugar.".PHP_EOL.
+                    " 0. Salir de la aplicacion.".PHP_EOL);
+switch($opcion){
+    case 1: 
+        //ingresar el numero
+        $numero1= readline("Ingrese un numero: ");
+        //convertilo a un array
+        break;
+    case 0:
+        echo "Ha decidido salir del programa".PHP_EOL;
+        break;
+    default:
+        echo "Opcion no valida, vuelva a intentarlo".PHP_EOL;           
+}
+}while ($opcion!=0);
 
 
 //se  lo comparara con numeros de amstrong en base a su logica
