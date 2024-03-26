bd8ea295adea5d6a33a5d0e8fe58a74a5cde273f
diega <diegochiola@gmail.com>
2024-03-26 10:41:33 +0100

    convert $numero usuario into array, count items in array, then loop through the array

--- Katas_php_2024/Kata_21_03/index.php
+++ Katas_php_2024/Kata_21_03/index.php
@@ -1,21 +1,53 @@
 <?php
 
 //Kata numero de amstrong
-//el usuario ingresara un numero
-$numeroUsuario=153;
-$cifras=0;
+
+
+//se  lo comparara con numeros de amstrong en base a su logica
+//logica con booleano. true o false
+function esAmstrong($numeroUsuario){
+    if ($numeroUsuario != 0){
+        //1ero necesito convertir a un array, el numero inglesado.
+        $arrayDigitos = str_split($numeroUsuario);
+        //2do contar los digitos (elementos que hay en el array)
+        $cantidadDigitos = count($arrayDigitos);
+        echo $cantidadDigitos;
+
+        $comprobacion = 0;
+        //recorrer el array y hacer el calculo
+        foreach($arrayDigitos as $digito){
+            $comprobacion += pow($digito, $cantidadDigitos);
+        }
+
+        //$cifra = strlen($numeroUsuario);
+        //echo "la cantidad de digitos es:" . strlen($numeroUsuario). PHP_EOL;
+        //separar el numero en digitos
+        //$arrayNumero = str_split($count);
+        //recorrer el numero y exponenciarlo por $cifra
+        //$comprobacion= pow($base,$exp);
+
+
+        //$comprobacion= (pow(1,$cifra) + pow(5,$cifra)+ pow(3,$cifra));
+        //echo $comprobacion . PHP_EOL;
+
+    }
+}
+//echo esAmstrong(153);
+
+
+//devolvera la respuesta si o no
+
 
 //ingresar el numero por temrinal:
-
 do{
-    $opcion = readline("Bienvenido a numero de Amstrong.".PHP_EOL.
-                    "Digite una opcion: ".PHP_EOL.
-                    " 1. Jugar.".PHP_EOL.
-                    " 0. Salir de la aplicacion.".PHP_EOL);
+    $opcion = readline("Bienvenido al verificador de números de Armstrong." . PHP_EOL .
+                        "Por favor, seleccione una opción:" . PHP_EOL .
+                        "1. Verificar un número." . PHP_EOL .
+                        "0. Salir del programa." . PHP_EOL);
 switch($opcion){
     case 1: 
         //ingresar el numero
-        $numero1= readline("Ingrese un numero: ");
+        $numeroUsuario= readline("Ingrese un numero: ");
         //convertilo a un array
         break;
     case 0:
@@ -25,25 +57,3 @@ switch($opcion){
         echo "Opcion no valida, vuelva a intentarlo".PHP_EOL;           
 }
 }while ($opcion!=0);
-
-
-//se  lo comparara con numeros de amstrong en base a su logica
-//logica con booleano. true o false
-function esAmstrong($numeroUsuario){
-    if ($numeroUsuario != 0){
-        //necesito contar las cifras del numero inglesado
-        $cifra = strlen($numeroUsuario);
-        echo "la cantidad de digitos es:" . strlen($numeroUsuario). PHP_EOL;
-        //separar el numero en digitos
-        //$arrayNumero = str_split($count);
-        //recorrer el numero y exponenciarlo por $cifra
-        //$comprobacion= pow($base,$exp);
-        $comprobacion= (pow(1,$cifra) + pow(5,$cifra)+ pow(3,$cifra));
-        echo $comprobacion . PHP_EOL;
-
-    }
-}
-echo esAmstrong(153);
-
-
-//devolvera la respuesta si o no
