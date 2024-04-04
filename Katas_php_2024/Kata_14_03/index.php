4a20d20d7ea97354e4d53d56ee9d68035081ba74
diega <diegochiola@gmail.com>
2024-04-04 19:09:14 +0200

    working properly

--- Katas_php_2024/Kata_14_03/index.php
+++ Katas_php_2024/Kata_14_03/index.php
@@ -13,14 +13,16 @@ function sumarDigitos($numerosIngresados, $numeroObjetivo){
     return $sumatotal == $numeroObjetivo;
 }
 //simplificar codigo (refactor)
-function ingresarNumeros($contador) {
+function ingresarNumeros($contador, $mostrarSumaParcial = true) {
     $numerosIngresados = [];
     for ($i = 1; $i <= $contador; $i++) {
         $numero = readline("Ingrese el número " . $i . ": ");
         $numerosIngresados[] = $numero;
         echo "Tu contador está en: " . $i . "\n";
-        $sumaParcial = array_sum(array_slice($numerosIngresados, 0, $i));
-        echo "Suma parcial: " . $sumaParcial . "\n";
+        if($mostrarSumaParcial){
+            $sumaParcial = array_sum(array_slice($numerosIngresados, 0, $i));
+            echo "Suma parcial: " . $sumaParcial . "\n";
+        }
     }
     return $numerosIngresados;
 }
@@ -35,50 +37,18 @@ do{
 
     switch($opcion){
         case 1:
-            // Freno 1
-                
-                echo "El objetivo es: " . $numeroObjetivo1 . "\n";
-                $numerosIngresados = ingresarNumeros(10);
-                $freno1 = sumarDigitos($numerosIngresados, $numeroObjetivo1);
-                echo $freno1 ? "Felicitaciones! Has frenado el primer freno del tren!\n" : "Has fallado!\n";
-                
-                if($freno1 && !$freno2){
-                    echo "El objetivo es: " . $numeroObjetivo2 . "\n";
-                    $numerosIngresados = ingresarNumeros(10);
-                    $freno2 = sumarDigitos($numerosIngresados, $numeroObjetivo2);
-                    echo $freno2 ? "Felicitaciones! Has salvado el tren!\n" : "Has fallado!\n";  
-                }
-        
-            /*
-            $numerosIngresados = [];
-            //pedir numero por terminal
-            $contador=10;
-            for ($i = 1; $i <= $contador; $i++) {
-                $numero = readline("Ingrese el número " . $i . ": ");
-                $numerosIngresados[] = $numero;
-                echo "Tu contador esta en: " . $i . "\n";
-                //suma parcial de numero ingresados
-                $sumaParcial = array_sum(array_slice($numerosIngresados, 0, $i));
-                echo "Suma parcial: " . $sumaParcial . " / " . $numeroObjetivo1 . "\n";
-            }
-            //freno 1
-            if ($opcion == 1 && !$freno1) {
-                $tren = new SalvarTren();
-                $freno1= $tren->sumarDigitos($numerosIngresados, $numeroObjetivo);
-                echo $freno1 ? "Felicitaciones! Has frenado el primer freno del tren!\n" : "Has fallado!\n";
-            }else if($freno1){
+            // Freno 1    
+            echo "El objetivo es: " . $numeroObjetivo1 . "\n";
+            $numerosIngresados = ingresarNumeros(10);
+            $freno1 = sumarDigitos($numerosIngresados, $numeroObjetivo1);
+            echo $freno1 ? "Felicitaciones! Has frenado el primer freno del tren!\n" : "Has fallado!\n";
+            //freno 2
+            if($freno1 && !$freno2){
                 echo "El objetivo es: " . $numeroObjetivo2 . "\n";
-                for ($i = 1; $i <= $contador; $i++) {
-                    $numero = readline("Ingrese el número " . $i . ": ");
-                    $numerosIngresados[] = $numero;
-                }
-                $tren = new SalvarTren();
-                $freno2 = $tren->sumarDigitos($numerosIngresados, $numeroObjetivo);
-                echo $freno2 ? "Felicitaciones! Has salvado el tren!\n" : "Has fallado!\n";
-            }else{
-                echo "Has fallado!\n";
+                $numerosIngresados = ingresarNumeros(10, false);
+                $freno2 = sumarDigitos($numerosIngresados, $numeroObjetivo2);
+                echo $freno2 ? "Felicitaciones! Has salvado el tren!\n" : "Has fallado!\n";  
             }
-            */
             break;
         case 0:
             echo "Ha decidido salir del programa". "\n";
@@ -88,6 +58,6 @@ do{
     }
    
   
-}while ($opcion!=0);
+}while ($opcion != 0);
 
 ?>
